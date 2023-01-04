f39d2a84f76b778b73cb0158211269cd3bf8c29f
Cleiber Melo <cleibernerymelo@gmail.com>
2023-01-04 14:40:58 +0000

    Add project files.

---
+++ E07_SomarDoisNumerosDiferentesZero/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E07_SomarDoisNumerosDiferentesZero
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int resultadoSoma = 0;
+            Console.WriteLine("Escreva o primeiro número: ");
+            var numero1 = Console.ReadLine();
+            Console.WriteLine("Escreva o segundo número: ");
+            var numero2 = Console.ReadLine();
+            if ((Convert.ToInt32(numero1) != 0) && ((Convert.ToInt32(numero2) != 0)))
+            {
+                int resultadoSoma = Convert.ToInt32(numero1) + Convert.ToInt32(numero2);
+                Console.WriteLine(resultadoSoma);
+            }
+            else
+            {
+                Console.WriteLine("Os números informados, não são diferentes de zero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
